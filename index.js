e1621dc05ad3fd63186a3ceed140029f65ec23ff
Kimberly Le <91621873+lekimberly17@users.noreply.github.com>
2022-03-16 16:00:31 +0000

    adds user list

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+
 const port = 3000;
+
+var data = require('./data/test.json');
 
 app.set('view engine', 'ejs');
 
@@ -12,6 +15,26 @@ app.get('/', (req, res) => {
   var title = 'Home Page';
   res.render('pages/index', {title:title});
 });
+
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
 
 app.get('/cannoli', (req, res) => {
   var title = 'Cannoli Recipe';
@@ -30,4 +53,5 @@ app.get('/tiramisu', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(data);
 });
